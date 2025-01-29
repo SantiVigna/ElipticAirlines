d341decb226bbebc3bae9ae703885ea82ef3b578
SantiVigna <ynotvigna@gmail.com>
2025-01-29 15:30:48 +0100

    feat: make api routes for flights

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Models\Airplane;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\FlightController;
 use App\Http\Controllers\Api\AirplaneController;
 
 Route::get('/user', function (Request $request) {
@@ -14,4 +15,11 @@ Route::post('/airplanes', [AirplaneController::class, 'store'])->name('airplanes
 Route::get('/airplanes/{id}', [AirplaneController::class, 'show'])->name('airplanesShow');
 Route::put('/airplanes/{id}', [AirplaneController::class, 'update'])->name('airplanesUpdate');
 Route::delete('/airplanes/{id}', [AirplaneController::class, 'destroy'])->name('airplanesDestroy');
+
+Route::get('/flights', [FlightController::class, 'index'])->name('flightsIndex');
+Route::post('/flights', [FlightController::class, 'store'])->name('flightsStore');
+Route::get('/flights/{id}', [FlightController::class, 'show'])->name('flightsShow');
+Route::put('/flights/{id}', [FlightController::class, 'update'])->name('flightsUpdate');
+Route::delete('/flights/{id}', [FlightController::class, 'destroy'])->name('flightsDestroy');
+
 
