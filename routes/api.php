fed4591df87fd1a4b2de1ee2895b3aa0a1f3a79c
SantiVigna <ynotvigna@gmail.com>
2025-02-04 14:23:49 +0100

    feat: fix wrong naming on routes from api

--- routes/api.php
+++ routes/api.php
@@ -10,16 +10,16 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/airplanes', [AirplaneController::class, 'index'])->name('airplanesIndex');
-Route::post('/airplanes', [AirplaneController::class, 'store'])->name('airplanesStore');
-Route::get('/airplanes/{id}', [AirplaneController::class, 'show'])->name('airplanesShow');
-Route::put('/airplanes/{id}', [AirplaneController::class, 'update'])->name('airplanesUpdate');
-Route::delete('/airplanes/{id}', [AirplaneController::class, 'destroy'])->name('airplanesDestroy');
+Route::get('/airplanes', [AirplaneController::class, 'index'])->name('airplanesApiIndex');
+Route::post('/airplanes', [AirplaneController::class, 'store'])->name('airplanesApiStore');
+Route::get('/airplanes/{id}', [AirplaneController::class, 'show'])->name('airplanesApiShow');
+Route::put('/airplanes/{id}', [AirplaneController::class, 'update'])->name('airplanesApiUpdate');
+Route::delete('/airplanes/{id}', [AirplaneController::class, 'destroy'])->name('airplanesApiDestroy');
 
-Route::get('/flights', [FlightController::class, 'index'])->name('flightsIndex');
-Route::post('/flights', [FlightController::class, 'store'])->name('flightsStore');
-Route::get('/flights/{id}', [FlightController::class, 'show'])->name('flightsShow');
-Route::put('/flights/{id}', [FlightController::class, 'update'])->name('flightsUpdate');
-Route::delete('/flights/{id}', [FlightController::class, 'destroy'])->name('flightsDestroy');
+Route::get('/flights', [FlightController::class, 'index'])->name('flightsApiIndex');
+Route::post('/flights', [FlightController::class, 'store'])->name('flightsApiStore');
+Route::get('/flights/{id}', [FlightController::class, 'show'])->name('flightsApiShow');
+Route::put('/flights/{id}', [FlightController::class, 'update'])->name('flightsApiUpdate');
+Route::delete('/flights/{id}', [FlightController::class, 'destroy'])->name('flightsApiDestroy');
 
 
