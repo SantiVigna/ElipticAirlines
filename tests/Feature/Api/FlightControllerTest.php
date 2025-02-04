fe8015691ae9424d3fcb1ffe60214d0b52ec7b9a
SantiVigna <ynotvigna@gmail.com>
2025-02-04 15:06:26 +0100

    feat: test if can create a flight

--- tests/Feature/Api/FlightControllerTest.php
+++ tests/Feature/Api/FlightControllerTest.php
@@ -27,4 +27,25 @@ class FlightControllerTest extends TestCase
         $response->assertStatus(200)
                  ->assertJsonFragment($data);
     }
+
+    public function test_CheckIfCanCreateAFlight() {
+        $this->seed(DatabaseSeeder::class);
+
+        $response = $this->postJson(route('flightsApiStore'), [
+            'flight_number' => 'ES-A19',
+            'departure' => 'Madrid',
+            'arrival' => 'Barcelona',
+            'departure_time' => '01/02/2023',
+            'arrival_time' => '01/02/2023',
+            'distance' => 620,
+            'price' => 50,
+            'airplane_id' => 8,
+        ]);
+
+        $response->assertStatus(201)
+                 ->assertJsonFragment([
+                    'flight_number' => 'ES-A19'
+                ]);
+    }
+
 }
