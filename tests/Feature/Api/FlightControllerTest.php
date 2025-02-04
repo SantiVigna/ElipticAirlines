f3560375f3ea65d0f74d8f70d02ec32391118f76
SantiVigna <ynotvigna@gmail.com>
2025-02-04 14:45:20 +0100

    feat: test getting just one flight

--- tests/Feature/Api/FlightControllerTest.php
+++ tests/Feature/Api/FlightControllerTest.php
@@ -10,12 +10,21 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class FlightControllerTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     public function test_CheckIfCanGetAllTheFlights() {
         $this->seed(DatabaseSeeder::class);
 
         $response = $this->getJson(route('flightsApiIndex'));
         $response->assertStatus(200)
                  ->assertJsonCount(10);
+    }
+
+    public function test_CheckIfCanGetOnlyOneFlight() {
+        $this->seed(DatabaseSeeder::class);
+
+        $response = $this->getJson(route('flightsApiShow', 1));
+        $data = ['id' => 1];
+        $response->assertStatus(200)
+                 ->assertJsonFragment($data);
     }
 }
