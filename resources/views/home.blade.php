d86e4bab99ef92605c6386ac388e2e51f54642de
SantiVigna <ynotvigna@gmail.com>
2025-02-04 13:12:59 +0100

    feat: Fix language in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,9 @@
 @section('content')
 
 <div class="flex flex-col items-center justify-center h-screen">
-    <h1 class="text-4xl font-bold text-gray-800">¡Bienvenido a Eliptic Airlines!</h1>
-    <p class="text-gray-600">Tu aerolínea de confianza</p>
-    <a href="{{ route('flightsIndex') }}" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md">Ver vuelos</a>
+    <h1 class="text-4xl font-bold text-gray-800">Welcome to Eliptic Airlines</h1>
+    <p class="text-gray-600">The best airline to reach your destination</p>
+    <a href="{{ route('flightsIndex') }}" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md">See Flights</a>
 
 
 </div>
