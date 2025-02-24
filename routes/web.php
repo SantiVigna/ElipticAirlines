846abece5fec49939f09d1a4aee6615c2740b4a9
SantiVigna <ynotvigna@gmail.com>
2025-02-24 13:03:41 +0100

    feat: add airplane routes and fix naming of some routes

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,16 @@ Route::get('/contact', function () {
 Route::get('flights' , [FlightController::class, 'index'])->name('flightsIndex');
 Route::get('flights/create' , [FlightController::class, 'create'])->name('flightsForm');
 Route::post('flights/store' , [FlightController::class, 'store'])->name('flightsStore');
-Route::get('flights/{id}' , [FlightController::class, 'edit'])->name('flightEditForm');
-Route::patch('flights/{id}' , [FlightController::class, 'update'])->name('flightUpdate');
-Route::delete('flights/{id}' , [FlightController::class, 'destroy'])->name('flightDelete');
+Route::get('flights/edit/{id}' , [FlightController::class, 'edit'])->name('flightEditForm');
+Route::patch('flights/update/{id}' , [FlightController::class, 'update'])->name('flightUpdate');
+Route::delete('flights/delete/{id}' , [FlightController::class, 'destroy'])->name('flightDelete');
 
 Route::get('airplanes' , [AirplaneController::class, 'index'])->name('airplanesIndex');
 Route::get('airplanes/create' , [AirplaneController::class, 'create'])->name('airplanesForm');
 Route::post('airplanes/store' , [AirplaneController::class, 'store'])->name('airplanesStore');
+Route::get('airplanes/edit/{id}' , [AirplaneController::class, 'edit'])->name('airplaneEditForm');
+Route::patch('airplanes/update/{id}' , [AirplaneController::class, 'update'])->name('airplaneUpdate');
+Route::delete('airplanes/delete/{id}' , [AirplaneController::class, 'destroy'])->name('airplaneDelete');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
