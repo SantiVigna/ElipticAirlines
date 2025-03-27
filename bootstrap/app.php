8c3511f73f3b210fa7cb0e8e6133d5007e2e7198
Santino <ynotvigna@gmail.com>
2025-03-27 22:37:46 +0100

    feat: put an alias for apiAdmin middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ApiAdmin;
 use App\Http\Middleware\CheckAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -15,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'admin' => CheckAdmin::class,
+            'apiAdmin' => ApiAdmin::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
