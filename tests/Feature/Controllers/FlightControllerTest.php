7a9d04dc3f2e2069bf30dc9114f63b199ab65c0f
Santino <ynotvigna@gmail.com>
2025-03-28 12:54:51 +0100

    test: Flight functions with admin login

--- tests/Feature/Controllers/FlightControllerTest.php
+++ tests/Feature/Controllers/FlightControllerTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature\Controllers;
 
 use Tests\TestCase;
+use App\Models\User;
 use App\Models\Flight;
+use App\Models\Airplane;
 use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -11,6 +13,16 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class FlightControllerTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected $admin;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->admin = User::factory()->create([
+            'isAdmin' => 1,
+        ]);
+    }
     
     public function test_CheckIfCanGetFlightView()
     {
@@ -19,5 +31,57 @@ class FlightControllerTest extends TestCase
         $response = $this->get(route('flightsIndex'));
 
         $response->assertViewIs('flights');        
+    }
+
+    public function test_CheckIfFlightCreateFormLoadsCorrectly()
+    {
+        $this->seed(DatabaseSeeder::class);
+
+        $response = $this->actingAs($this->admin)
+                         ->get(route('flightsForm')); 
+
+        $response->assertStatus(200)
+                 ->assertViewIs('createFlightForm');
+    }
+
+    public function test_CheckIfFlightEditFormLoadsCorrectly()
+    {
+        $this->seed(DatabaseSeeder::class);
+
+
+
+        $response = $this->actingAs($this->admin)
+                         ->get(route('flightEditForm', 1)); 
+
+        $response->assertStatus(200)
+                 ->assertViewIs('editFlightForm');
+    }
+
+    public function test_CheckIfCanStoreAFlight()
+    {
+        $this->seed(DatabaseSeeder::class);
+
+        $response = $this->actingAs($this->admin)
+                         ->post(route('flightsStore'), [
+            'flight_number' => 'ES-A19',
+            'departure' => 'Madrid',
+            'arrival' => 'Barcelona',
+            'departure_time' => '01/02/2023',
+            'arrival_time' => '01/02/2023',
+            'distance' => 620,
+            'price' => 50,
+            'airplane_id' => 1,
+        ]);
+
+        $response->assertRedirect(route('flightsIndex'));
+    }
+
+    public function test_CheckIfCanDeleteAFlight() {
+        $this->seed(DatabaseSeeder::class);
+
+        $response = $this->actingAs($this->admin)
+                         ->delete(route('flightDelete', 1));
+
+        $response->assertRedirect(route('flightsIndex'));
     }
 }
